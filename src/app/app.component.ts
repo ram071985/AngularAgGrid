86d1c0506e62d217c7482f412b2a4417693ebf80
reidmuchow <reid.muchow@clickbond.com>
2021-07-26 15:25:56 -0700

    initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ag-grid-app';
+
+  columnDefs = [
+    { field: 'make', sortable: true, filter: true },
+    { field: 'model', sortable: true, filter: true },
+    { field: 'price', sortable: true, filter: true },
+  ];
+
+  rowData!: Observable<any[]>;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.rowData = this.http.get<any[]>(
+      'https://www.ag-grid.com/example-assets/small-row-data.json'
+    );
+  }
 }
